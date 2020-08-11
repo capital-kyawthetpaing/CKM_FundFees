50f846cd2987dcbe1b09a6014826f06d9ae9dab2
kyawthetpaing <kyawthetpaing@mm.gmail.com>
2020-08-11 11:03:37 +0630

    ktp

---
+++ Models/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace Models
+{
+    public class BaseModel
+    {
+        public string Mode { get; set; }
+        public string SPName { get; set; }
+        public string CreatedBy { get; set; }
+        public string CreatedDate { get; set; }
+        public SqlParameter[] Sqlprms { get; set; }
+        public string inputdate { get; set; }
+    }
+}
