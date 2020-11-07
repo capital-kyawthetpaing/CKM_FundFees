85992d8aee1e154fe21e1c5921dbccb595c4e099
acer <acer@DESKTOP-3FJTCQT>
2020-11-07 09:40:10 +0630

    by syp

---
+++ Models/EntryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class EntryModel:BaseModel
+    {
+        public string PageName { get; set; }
+        public string CanRead { get; set; }
+        public string CanWrite { get; set; }
+        public string CanDelete { get; set; }
+        public string PageID { get; set; }
+    }
+}
